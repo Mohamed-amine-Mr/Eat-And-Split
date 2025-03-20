f3928b22569b3aec03629d765e5c41a8c14614ad
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-03-20 17:26:25 +0000

    - adding default img url to the image piece of state , - adding handleSubmit function to preventing default reload when we submit the form

--- src/App.jsx
+++ src/App.jsx
@@ -82,10 +82,14 @@ function Friend({ friend }) {
 
 function FormAddFriend() {
   const [name, setName] = useState("");
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=118036");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
 
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👫 Friend name</label>
       <input
         type="text"
