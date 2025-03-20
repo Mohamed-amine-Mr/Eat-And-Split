1e333e8609c9443d66c37a5b88428105ecd1b416
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-03-20 18:44:10 +0000

    feat: Add friend creation functionality via form submission
    Implement handleAddFriend function in App component to update friends state
    Pass onAddFriend callback prop to FormAddFriend component
    Modify FormAddFriend to use the callback when form is submitted
    Connect form submission with state updates to display new friends in FriendsList

--- src/App.jsx
+++ src/App.jsx
@@ -36,12 +36,16 @@ const App = () => {
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
   }
+  // lets gives Form ability to add new friend
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+  }
 
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList friends={friends} />
-        {showAddFriend && <FormAddFriend />}
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "close" : "Add friend"}
         </Button>
@@ -82,13 +86,15 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48?u=118036");
 
   function handleSubmit(e) {
-    if (!name || !image) return;
     e.preventDefault();
+
+    if (!name || !image) return;
+
     const id = crypto.randomUUID(); // for generating rondom Id
     const newFriend = {
       id,
@@ -96,7 +102,9 @@ function FormAddFriend() {
       image: `${image}?=${id}`,
       balance: 0,
     };
-    console.log(newFriend);
+
+    onAddFriend(newFriend);
+
     setName("");
     setImage("https://i.pravatar.cc/48?u=118036");
   }
